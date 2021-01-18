6ff1ba26f9a8ba40510b51b7f4927531b0c1c27f
AlexLazy <alexlazydev@gmail.com>
2021-01-18 15:47:21 +0300

    fix db authorization

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('tasker', 'root', 'root', {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_LOGIN, process.env.DB_PASSWORD, {
+  host: process.env.DB_URI,
   dialect: 'mysql'
 });
 
